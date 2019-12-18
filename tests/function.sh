80736184ea88cf0c53126e6d2e39cb38cbff9b42
ImOverlord <thibault-alexandre.faure@epitech.eu>
2019-12-18 17:43:00 +0100

    Testing CI

--- tests/function.sh
+++ tests/function.sh
@@ -4,6 +4,11 @@ chown -R $(stat -c "%u:%g" /home/node/) /home/node
 npm ci
 npm run build
 npm link
-boost --help
+if command ; then
+    echo "Boost Installed"
+else
+    echo "Failed to insall Boost"
+    exit 1
+fi
 chown -R $(stat -c "%u:%g" /home/node/) /home/node
 npm run test-func
